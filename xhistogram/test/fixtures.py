c9216a4004e9033bef8abc05392603cf94395fdd
Thomas Nicholas <tegn500@york.ac.uk>
2021-05-25 14:46:40 -0400

    new fixtures

--- xhistogram/test/fixtures.py
+++ xhistogram/test/fixtures.py
@@ -1,5 +1,11 @@
+import random
+import string
+
+import pytest
 import dask
 import dask.array as dsa
+import numpy as np
+import xarray as xr
 
 
 def empty_dask_array(shape, dtype=float, chunks=None):
@@ -12,3 +18,34 @@ def empty_dask_array(shape, dtype=float, chunks=None):
         a = a.rechunk(chunks)
 
     return a
+
+
+@pytest.fixture(scope="module")
+def dataarray_factory():
+    def _dataarray_factory(shape=(5,20)):
+        data = np.random.randn(*shape)
+        dims = [random.choice(string.ascii_lowercase) for ax in shape]
+        da = xr.DataArray(
+            data, dims=dims, name="T"
+        )
+        return da
+    return _dataarray_factory
+
+
+@pytest.fixture(scope="module")
+def dataset_factory():
+    """Random dataset with every variable having the same shape"""
+    def _dataset_factory(ndim=2, n_vars=2):
+        shape = (8, 9, 10, 11)[:ndim]
+        dims = [random.choice(string.ascii_lowercase) for ax in shape]
+        ds = xr.Dataset()
+        for _ in range(n_vars):
+            name = random.choice(string.ascii_uppercase)
+            data = np.random.randn(*shape)
+            da = xr.DataArray(
+                data, dims=dims, name=name
+            )
+            ds[name] = da
+        return ds
+
+    return _dataset_factory
